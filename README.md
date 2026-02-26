089af0e8ec8863c87aab87d3cb21e5a6e22d1075
Ratthasart Rojjanai <feevfeev2@gmail.com>
2026-02-26 11:34:17 +0700

    up

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # test_DB
+
 # 1. สร้างโฟลเดอร์โปรเจคก่อน ด้วยมือนักศึกษา
+
 # 2. สร้างไฟล์ package.json (ไฟล์สำหรับเก็บรายชื่อ Library ที่ใช้)
+
     npm init -y
+
 # 3. ติดตั้ง Express.js
+
     npm i express
     npm i mysql2
     npm i cors
+    npm install multer
+
 # i ย่อมาจาก install
 
-# 4. การรัน index.js
-    node .
-# อิงจาก "main": "index.js" ที่อยู่ใน package.json
+# 4. การรัน server.js
+
+"scripts": {
+    "start": "node server.js"
+}
+npm start
+
+# อิงจาก "main": "server.js" ที่อยู่ใน package.json
--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 const express = require("express");
 const app = express();
+const multer = require("multer");
 const pool = require("./DB");
 const PORT = 3000;
 
 app.use(express.json());
-
-const rateLimit = require("express-rate-limit");
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // ช่วงเวลา 15 นาที
-  max: 100 // จำกัด 100 คำขอต่อ IP ในช่วงเวลานี้
-});
-
-app.use("/api/", limiter);
+app.use(express.static("public"));
 
 app.get("/api/products", async (req, res) => {
   try {
     const sql =
-      "SELECT products.id, products.name, products.price, categories.name AS category_name " +
+      "SELECT products.id, products.name, products.price, products.image_url, categories.name AS category_name " +
       "FROM products JOIN categories ON products.category_id = categories.id";
     const [products] = await pool.query(sql);
     res.json(products);
@@ -26,17 +19,55 @@ app.get("/api/products", async (req, res) => {
   }
 });
 
+const storage = multer.diskStorage({
+  destination: "./public/images/",
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+app.post("/api/products", upload.single("image"), async (req, res) => {
+  try {
+    const { name, price, category_id } = req.body;
+    const image_url = req.file
+      ? `/images/${req.file.filename}`
+      : "/images/no-image.png";
+
+    const sql = `INSERT INTO products (name, price, image_url, category_id) VALUES (?, ?, ?, ?)`;
+    await pool.query(sql, [name, price, image_url, category_id]);
+
+    res.json({ message: "อัปโหลดและบันทึกสําเร็จ!" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.delete("/api/products/:id", (req, res) => {
+  const productId = req.params.id; // รับค่า ID จาก URL
+  const sql = "DELETE FROM products WHERE id = ?";
+
+  pool.query(sql, [productId], (err, result) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.send({ message: "Product deleted successfully" });
+  });
+});
+
 function render(request, response) {
   let url = request.url;
   url = url.endsWith("/") ? url : url + "/";
   let fileName = "public/";
-    switch (url) {
+  switch (url) {
     case "/":
       fileName += "index.html";
-        break;
-    }
+      break;
+  }
   response.sendFile(__dirname + "/" + fileName);
 }
 app.get("/", render);
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`),
+);
